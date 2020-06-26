272d679d645380cd4ed189008219272e60106f48
Nicolas Jeanselme <41996571+njeanselme@users.noreply.github.com>
2020-06-26 12:20:42 +0200

    Update fortiguard-to-infoblox-csp.py

--- fortiguard-to-infoblox-csp.py
+++ fortiguard-to-infoblox-csp.py
@@ -1,94 +1,126 @@
-from stix.core import STIXPackage 
 import requests
 import base64
 import json
 import re
 import math
-import gzip
 import logging
-
+import time
+import gzip
+import ipaddress
 
 #########################################################			
 logging.basicConfig(handlers = [logging.FileHandler('fortiguard-to-infoblox-csp.log'), logging.StreamHandler()],level=logging.DEBUG,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 tide_apikey = ''
 csp_apikey  = ''
-fortiguard_apikey= '' #not used yet
+fortiguard_apikey= ''
+use_already_downloaded_IOC_files = False
 
 #########################################################
 
-def getTIDEIOCs(url,tide_apikey):
+def is_fqdn(hostname):
+    """
+    :param hostname: string
+    :return: bool
+    """
+    #  Remove trailing dot
+    try:  # Is this necessary?
+        if hostname[-1] == '.':
+            hostname = hostname[0:-1]
+    except IndexError:
+        return False
+
+    #  Check total length of hostname < 253
+    if len(hostname) > 253:
+        return False
+
+    #  Split hostname into list of DNS labels
+    hostname = hostname.split('.')
+
+    #  Define pattern of DNS label
+    #  Can begin and end with a number or letter only
+    #  Can contain hyphens, a-z, A-Z, 0-9
+    #  1 - 63 chars allowed
+    fqdn = re.compile(r'^[a-z0-9]([a-z-0-9-]{0,61}[a-z0-9])?$', re.IGNORECASE)
+
+    #  Check if length of each DNS label < 63
+    #  Match DNS label to pattern
+    for label in hostname:
+        if len(label) > 63:
+            return False
+        if not fqdn.match(label):
+            return False
+
+    #  Found no errors, returning True
+    return True
+    
+#########################################################
+
+def getTIDEIOCs(test_mode, ioctype, url,tide_apikey):
 	data ={}
-	method='GET'
-	auth = base64.encodebytes(('%s:%s' % (tide_apikey,' ')).encode()).decode().replace('\n', '').strip()
-	headers = {
-		'Authorization':'Basic %s' % auth,
-		'Content-Type':'application/x-www-form-urlencoded',
-		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
-		'Cache-Control': 'no-cache'
-	}
-	response = requests.get(url, headers=headers, cookies=None, verify=True, timeout=(600,600), stream=True)
+	filename = './tide_'+ ioctype + '_' + time.strftime("%Y%m%d") +'.json'
 	
-	if response.encoding is None:
-		response.encoding = 'utf-8'
-		for line in response.iter_lines(decode_unicode=True):
-			if line:
-				try:
-					r_json=json.loads(line)
-				except:
-					raise Exception('Unable to load into a json format')
+	if not test_mode:
+		method='GET'
+		auth = base64.encodebytes(('%s:%s' % (tide_apikey,' ')).encode()).decode().replace('\n', '').strip()
+		headers = {
+			'Authorization':'Basic %s' % auth,
+			'Content-Type':'application/x-www-form-urlencoded',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
+			'Cache-Control': 'no-cache'
+		}
+		response = requests.get(url, headers=headers, cookies=None, verify=True, timeout=(600,600), stream=True)
+		with open(filename, 'wb') as file:
+			for chunk in response.iter_content(chunk_size=8192):
+				file.write(chunk)
+
+	file= open(filename, 'r')
 	
-				if r_json['type'] == 'HOST':
-					data[r_json['host']] = ''
-				elif r_json['type'] == 'IP':
-					data[r_json['ip']] = ''
+	for line in file:
+		try:
+			r_json=json.loads(line)
+		except:
+			raise Exception('Unable to load into a json format')
+
+		if r_json['type'] == 'HOST':
+			data[r_json['host']] = ''
+		elif r_json['type'] == 'IP':
+			data[r_json['ip']] = ''
+	
+	logging.info('Download ok, {} TIDE IOCs: {}'.format(ioctype,len(data)))
 	return data
 	
 #########################################################
 	
-def getFortiguardIOCs(file,fortiguard_apikey):
+def getFortiguardIOCs(test_mode, fortiguard_apikey):
 	data ={}
-	
-	'''
-	headers= {'Authorization': 'Token:{}'.format(fortiguard_apikey)}
-	url= 'https://premiumapi.fortinet.com/v1/cti/feed/stix?cc=us'
-	response = requests.get(url, headers=headers, cookies=None, verify=True, timeout=(600,600), stream=True)
-	url = response.json()[0]['data']
-	response = requests.get(url, headers=headers, cookies=None, verify=True, timeout=(600,600), stream=True)
-	
-	with gzip.open('/home/joe/file.txt.gz', 'rb') as file:
-	    file_content = file.read()
-	'''
-	
-	stix_package = STIXPackage.from_xml(file)
 
-	ttps={}
-	json_ttps = json.loads(stix_package.ttps.to_json())
-	if 'ttps' in json_ttps:
-		json_ttps=json_ttps['ttps']
-		for json_ttp in json_ttps:
-			if 'id' in json_ttp and 'title' in json_ttp:
-				ttps[json_ttp['id']]=json_ttp['title']
+	headers= {'Token': '{}'.format(fortiguard_apikey)}
+	filename = './fortinet_all_'+ time.strftime("%Y%m%d") +'.csv'
+	
+	if not test_mode:
+		url= 'https://premiumapi.fortinet.com/v1/cti/feed/csv?cc=all'
+		response = requests.get(url, headers=headers, cookies=None, verify=True, timeout=(600,600), stream=True)
+		
+		url = response.json()[0]['data']
+		response = requests.get(url, headers=headers, cookies=None, verify=True, timeout=(600,600), stream=True)
+		
+		open(filename, 'wb').write(gzip.decompress(response.content))
 
-	indicators = json.loads(stix_package.indicators.to_json())
-	for indicator in indicators:
-		IOC = {}
+	file=open(filename, 'r')
+	
+	for line in file:
+		line = line.strip()
 		try:
-			r_json = indicator['observable']['object']['properties']		
-			if 'type' in r_json and 'value' in r_json and r_json['type'] == 'Domain Name':
-				IOC['item'] = r_json['value']
-			elif 'ip_address' in r_json and 'address_value' in r_json['ip_address'] and 'value' in r_json['ip_address']['address_value']:
-				IOC['item'] = r_json['ip_address']['address_value']['value']
-				
-			if 'indicated_ttps' in indicator and len(indicator['indicated_ttps'])>0 and 'ttp' in indicator['indicated_ttps'][0] and 'idref' in indicator['indicated_ttps'][0]['ttp']:
-				ttp_id = indicator['indicated_ttps'][0]['ttp']['idref']
-				if ttp_id in ttps:
-					IOC['description'] = ttps[ttp_id]
-			
-			if 'item' in IOC:
-				data[IOC['item']]=IOC
+			if is_fqdn(line) or ipaddress.ip_address(line):
+				IOC = {}
+				IOC['item']=line
+				data[line] = IOC
 		except:
 			pass
+		
+
+	logging.info('Download ok, Fortinet IOCs: {}'.format(len(data)))
 
 	return data
 
@@ -96,7 +128,7 @@ def getFortiguardIOCs(file,fortiguard_apikey):
 
 def generate_new_IOC_list(TIDE_IOCs,Fortiguard_IOCs):
 	data ={}
-	diff = set(Fortiguard_IOCs).difference(set(TIDE_IOCs))	
+	diff = set(Fortiguard_IOCs).difference(set(TIDE_IOCs))
 	for k in diff:
 		data[k] = Fortiguard_IOCs[k]
 	
@@ -172,7 +204,7 @@ def update_to_csp(new_IOCs, csp_apikey):
 	number_of_named_list_to_create = max(0,math.ceil((named_list_capacity_required - named_lists_current_capacity) / max_records_per_csp_list))
 		
 	logging.info('named_lists_current_capacity = {}, named_list_capacity_required = {}, number_of_named_list_to_create = {}'.format(named_lists_current_capacity,named_list_capacity_required,number_of_named_list_to_create))
-	
+
 	
 	#Create the List ####################################
 	name_list_names=[0]
@@ -188,6 +220,7 @@ def update_to_csp(new_IOCs, csp_apikey):
 	#Update available Named Lists #######################	
 	list_of_named_lists = get_named_lists(named_list_prefix,headers)
 	
+	
 	#Add to list ########################################
 	i=0
 	for named_list in list_of_named_lists:
@@ -201,20 +234,24 @@ def update_to_csp(new_IOCs, csp_apikey):
 		json_to_add['items_described'] = IOCs_to_add_to_list
 		logging.info("Adding {} entries in named_list {}, {}".format(len(IOCs_to_add_to_list),named_list['name'],named_list['id']))
 		response = requests.post('https://csp.infoblox.com/api/atcfw/v1/named_lists/{}/items'.format(named_list['id']), headers=headers, data=json.dumps(json_to_add, indent=4, sort_keys=True), verify=True, timeout=(300,300))
+		try:
+			response.raise_for_status()
+		except requests.exceptions.HTTPError as e:
+			return "Error: " + str(e)
 
-	
 	#########################################################			
-
 
 hosts_url = 'https://api.activetrust.net/api/data/threats/state/host?data_format=ndjson'
 ips_url   = 'https://api.activetrust.net/api/data/threats/state/IP?data_format=ndjson'
 
-TIDE = getTIDEIOCs(hosts_url, tide_apikey)
-TIDE.update(getTIDEIOCs(  ips_url, tide_apikey))
+TIDE = {}
+TIDE = getTIDEIOCs(use_already_downloaded_IOC_files, 'host', hosts_url, tide_apikey)
+TIDE.update(getTIDEIOCs(use_already_downloaded_IOC_files, 'ip', ips_url, tide_apikey))
 
-Fortiguard_IOCs = getFortiguardIOCs('sample.stix',fortiguard_apikey)
+Fortiguard_IOCs = getFortiguardIOCs(use_already_downloaded_IOC_files, fortiguard_apikey)
 
 new_IOCs = generate_new_IOC_list(TIDE, Fortiguard_IOCs)
-logging.debug("IOCs in Fortiguard but not in TIDE:\n{}".format(json.dumps(new_IOCs, indent=4, sort_keys=True)))
+logging.debug('{:<20}  {:<50}'.format('-- Description --','-- IOC --'))
+[logging.debug('{:<20}  {:<50}'.format(new_IOCs[x].get('description',''),new_IOCs[x].get('item','')))  for x in set(new_IOCs)]
 
 update_to_csp(new_IOCs, csp_apikey)
